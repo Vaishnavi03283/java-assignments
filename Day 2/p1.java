a5b58a162d3725efd6f5826cfe8659a34f3354ce
Your Namevaishnavi03283 <you@example.comvaishnavij3112004gmail.com>
2024-10-01 21:44:52 +0530

    code added

---
+++ Day 2/p1.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main (String [] args){
+		int a=1;
+		while(a<=10){
+			System.out.print(a+" ");
+			a++;
+		}
+	}
+}
+
+
+
+OUTPUT:-
+
+1 2 3 4 5 6 7 8 9 10
---
+++ Day 2/p6.java
@@ -0,0 +1,9 @@
+class Demo{
+	public static void main(String [] args){
+		int a=1;
+		while(a%3==0 && a%7==0){
+			System.out.print("a ");
+			a++;
+		}
+	}
+}
