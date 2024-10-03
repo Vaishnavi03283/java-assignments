2abd0d6666731a219306aeb95aa656f5f1407d4a
Your Namevaishnavi03283 <you@example.comvaishnavij3112004gmail.com>
2024-10-03 13:20:52 +0530

    code added

---
+++ Day 4/p1.java
@@ -0,0 +1,16 @@
+class Demo{
+	public static void main(String [] args){
+		char ch='A';
+		for(int i=1;i<=5;i++){
+			System.out.print(ch+" ");
+			ch++;
+			ch++;
+		}
+	}
+}
+
+
+
+OUTPUT:-
+
+A C E G I
---
+++ Day 4/p2.java
@@ -0,0 +1,18 @@
+class Demo{
+	public static void main(String [] args){
+		for(int a=1;a<=5;a++){
+			if(a%2==1){
+				System.out.print((a*a*a)+" ");
+			}else{
+				System.out.print((a*a)+" ");
+			}
+		}
+	}
+}
+
+
+
+
+OUTPUT:-
+
+1 4 27 16 125
