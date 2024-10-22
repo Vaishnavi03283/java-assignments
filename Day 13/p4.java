8cafe8b07051e302d25c653ad3bb0644e5de9ed7
Your Namevaishnavi03283 <you@example.comvaishnavij3112004gmail.com>
2024-10-22 23:45:11 +0530

    codes added

--- Day 13/p4.java
+++ Day 13/p4.java
@@ -3,3 +3,19 @@
  Input: 5
  Output:
  The Factorial of 5 is: 120											*/
+
+
+
+class Demo{
+	public static void main(String [] args){
+		int a = 1;
+		for(int i=1; i<=5;i++){
+			a = a*i;
+		}
+		System.out.println("The Factorial of 5 is: "+a);
+	}
+}
+
+
+
+/*OUTPUT:- The Factorial of 5 is: 120								*/
